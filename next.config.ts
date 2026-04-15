a10188289a5802121fe125e497453b94b7a00550
Roberto Carpinteyro <66752420+robertocarpinteyro@users.noreply.github.com>
2026-04-15 14:30:31 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        // Intercepta la raíz de la ruta
+        source: '/propuestaZenith',
+        destination: 'https://propuesta-zenith-woad.vercel.app',
+      },
+      {
+        // Intercepta todos los archivos estáticos (JS, CSS, imágenes)
+        source: '/propuestaZenith/:path*',
+        destination: 'https://propuesta-zenith-woad.vercel.app/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
